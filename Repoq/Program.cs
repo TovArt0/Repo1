5a11ac771de3d193a3d7c6e5d9481c3c8c716f6f
TovArt0 <badestroyergod@mail.ru>
2022-11-11 22:47:37 +0700

    Программа для вычисления високосного года

---
+++ Repoq/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repoq
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int year;
+            string tmp;
+            string pause;
+            Console.WriteLine("Введите число:");
+            tmp = Console.ReadLine();
+            year = Convert.ToInt32(tmp);
+            Console.WriteLine($"Введенный год: {year}");
+
+            if (year % 4 == 0)
+            {
+                Console.WriteLine("Год високосный");
+            }
+            else
+            {
+                Console.WriteLine("Год не високосный");
+            }
+
+            pause = Console.ReadLine();
+
+
+
+        }
+    }
+ }
+
